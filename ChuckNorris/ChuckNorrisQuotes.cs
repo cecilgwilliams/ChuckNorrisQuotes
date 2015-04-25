9447560b3725e2bd3d155e5814fdac9ddab500cd
Cecil Williams <cecil.g.williams@gmail.com>
2015-04-25 18:45:01 -0500

    code cleanup

--- ChuckNorris/ChuckNorrisQuotes.cs
+++ ChuckNorris/ChuckNorrisQuotes.cs
@@ -1,43 +1,40 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace ChuckNorris
 {
     public class ChuckNorrisQuotes {
-        private List<string> quotes;
-        private IEnumerator<string> enumerator;
+        private readonly IEnumerator<string> _enumerator;
 
         public ChuckNorrisQuotes() {
-            this.quotes = new List<string>();
-            quotes.Add("Chuck Norris doesn't dial the wrong number. You answered the wrong phone.");
-            quotes.Add("Chuck Norris knows Victoria's secret.");
-            quotes.Add("If Chuck Norris was a Spartan in the movie 300, the movie would be called 1.");
-            quotes.Add("When Chuck Norris turned 18, his parents moved out.");
-            quotes.Add("When Chuck Norris swims in the ocean, the sharks are in a steel cage.");
-            quotes.Add("Chuck Norris will never have a heart attack. His heart isn't nearly foolish enough to attack him.");
-            quotes.Add("Chuck Norris once kicked a horse in the chin. Its descendants today are known as giraffes.");
-            quotes.Add("Chuck Norris doesn't breathe air. He holds air hostage.");
-            quotes.Add("Chuck Norris can delete the Recycling Bin.");
-            quotes.Add("Chuck Norris has already been to Mars. That's why there are no signs of life.");
-            quotes.Add("Chuck Norris can kill two stones with one bird.");
-            quotes.Add("Chuck Norris's calendar goes straight from March 31st to April 2. No one fools Chuck Norris.");
-            quotes.Add("Chuck Norris wears sunglasses so that his eyes won't hurt the sun.");
-            quotes.Add("When Chuck Norris works out he doesn't get stronger, the machine does.");
-            quotes.Add("Chuck Norris does not sleep; he waits.");
-            enumerator = quotes.GetEnumerator();
+            var quotes = new List<string> {
+                "Chuck Norris doesn't dial the wrong number. You answered the wrong phone.",
+                "Chuck Norris knows Victoria's secret.",
+                "If Chuck Norris was a Spartan in the movie 300, the movie would be called 1.",
+                "When Chuck Norris turned 18, his parents moved out.",
+                "When Chuck Norris swims in the ocean, the sharks are in a steel cage.",
+                "Chuck Norris will never have a heart attack. His heart isn't nearly foolish enough to attack him.",
+                "Chuck Norris once kicked a horse in the chin. Its descendants today are known as giraffes.",
+                "Chuck Norris doesn't breathe air. He holds air hostage.",
+                "Chuck Norris can delete the Recycling Bin.",
+                "Chuck Norris has already been to Mars. That's why there are no signs of life.",
+                "Chuck Norris can kill two stones with one bird.",
+                "Chuck Norris's calendar goes straight from March 31st to April 2. No one fools Chuck Norris.",
+                "Chuck Norris wears sunglasses so that his eyes won't hurt the sun.",
+                "When Chuck Norris works out he doesn't get stronger, the machine does.",
+                "Chuck Norris does not sleep; he waits."
+            };
+            _enumerator = quotes.GetEnumerator();
         }
 
         public string GetNextQuote() {
             
-            while (enumerator.MoveNext()) {
-                return enumerator.Current; 
+            if (_enumerator.MoveNext()) {
+                return _enumerator.Current;
             }
 
-            return quotes.First();
+            _enumerator.Reset();
+            _enumerator.MoveNext();
+            return _enumerator.Current;
         }
     }
 }
--- ChuckNorrisQuotesUnitTest/ChuckNorrisQuotesUnitTest.cs
+++ ChuckNorrisQuotesUnitTest/ChuckNorrisQuotesUnitTest.cs
@@ -1,66 +1,42 @@
-﻿using System;
-using ChuckNorris;
+﻿using ChuckNorris;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace ChuckNorrisQuotesUnitTest {
-    [TestClass]
-    public class ChuckNorrisQuotesUnitTest {
+namespace ChuckNorrisQuotesUnitTest 
+{
 
-        private ChuckNorrisQuotes chuckNorrisQuotes = new ChuckNorrisQuotes();
+    [TestClass]
+    public class ChuckNorrisQuotesUnitTest 
+    {
+
+        private readonly ChuckNorrisQuotes _chuckNorrisQuotes = new ChuckNorrisQuotes();
 
         [TestMethod]
-        public void TestFirst() {
-            var nextQuote = GetNextQuote();
+        public void TestFirst() 
+        {
+            var nextQuote = _chuckNorrisQuotes.GetNextQuote();
             Assert.AreEqual("Chuck Norris doesn't dial the wrong number. You answered the wrong phone.", nextQuote);
         }
 
         [TestMethod]
-        public void TestLast() {
-            var nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
+        public void TestLast() 
+        {
+            for (var i = 1; i < 15; i++) {
+                _chuckNorrisQuotes.GetNextQuote();
+            }
+            var nextQuote = _chuckNorrisQuotes.GetNextQuote();
             Assert.AreEqual("Chuck Norris does not sleep; he waits.", nextQuote);
         }
 
         [TestMethod]
-        public void TestRollBackToFirst() {
-            var nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
-            nextQuote = GetNextQuote();
+        public void TestRollBackToFirst() 
+        {
+            for (var i = 1; i < 16; i++) {
+                _chuckNorrisQuotes.GetNextQuote();
+            }
+            var nextQuote = _chuckNorrisQuotes.GetNextQuote();
             Assert.AreEqual("Chuck Norris doesn't dial the wrong number. You answered the wrong phone.", nextQuote);
+            nextQuote = _chuckNorrisQuotes.GetNextQuote();
+            Assert.AreEqual("Chuck Norris knows Victoria's secret.", nextQuote);
         }
-
-        private string GetNextQuote() {
-            
-            var nextQuote = chuckNorrisQuotes.GetNextQuote();
-            Console.WriteLine("Quote is " + nextQuote);
-            return nextQuote;
-        }
-
     }
 }
